907f0a8f0969b0ced6cbbb817765aec9d4472df5
arpan7sarkar <arpansakar@gmail.com>
2025-08-23 21:49:12 +0530

    used multiple lines of prompt for giving api a memory and learned about catched token

--- 2_PROMPTING/hello_prompting.js
+++ 2_PROMPTING/hello_prompting.js
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import {OpenAI} from 'openai'
 const client =new OpenAI();
 async function main() {
+    //These api calls are stateless (That means it doesnot remember anything) if one message is wrtten
+    //for fixing it we have to write multiple contexts for both user and assitant(in other words we need to write multipe messages)
+
     const response=await client.chat.completions.create({
         model:"gpt-4.1-nano",
         messages:[
-            {"role":"user","content":"Hey how are you?"}
+            {"role":"user","content":"My name is Arpan"},
+            {"role":"assistant","content":"Oh hello Arpan.How can i help you?"},
+            {"role":"user","content":"Whats my name?"},
+            {"role":"assistant","content":"Your name is Arpan, how can i help you?"},
+            {"role":"user","content":"Write a poem on my name in 2 lines"}/* output Arpan’s name, a gentle light so bright,  
+Guiding hearts with warmth and pure delight. */
+            /*You may ask here i am using so many tokens so the cost should be more right? but here is the catch. 
+            almost all tokens except the last one had been count as  cathced token its coth is 10 times cheaper then a normal token*/
         ]
     })
     console.log(response.choices[0].message.content);
